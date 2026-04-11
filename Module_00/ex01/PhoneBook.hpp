6600dc5c3dda114e0f31efff702c540cd9809c25
Julian Kersten <jukerste@f0r4s16.codam.nl>
2026-04-11 19:11:52 +0200

    start to modules

--- Module_00/ex01/PhoneBook.hpp
+++ Module_00/ex01/PhoneBook.hpp
@@ -6,11 +6,23 @@
 /*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 13:33:15 by jukerste          #+#    #+#             */
-/*   Updated: 2026/04/01 14:58:03 by jukerste         ###   ########.fr       */
+/*   Updated: 2026/04/11 17:25:55 by jukerste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 #define PHONEBOOK_H
+
+#include <iostream>
+#include <string>
+#include "Contact.hpp"
+
+class Contact
+{
+	private:
+	std::string contacts[8];
+	public:
+	
+};
 
 #endif
