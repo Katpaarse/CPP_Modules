0a792c7e9ea02216967da04925c3b2d2988e37f0
Julian Kersten <jukerste@f0r5s13.codam.nl>
2026-05-04 14:46:57 +0200

    deleted unused function declaration

--- Module_00/ex01/Contact.hpp
+++ Module_00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: jukerste <jukerste@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2026/04/01 14:57:39 by jukerste      #+#    #+#                 */
-/*   Updated: 2026/05/01 14:36:21 by jukerste      ########   odam.nl         */
+/*   Updated: 2026/05/04 14:42:07 by jukerste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ class Contact
 	std::string	_nickName;
 	std::string	_phoneNumber;
 	std::string _darkestSecret;
-	bool		_digitsOnly(std::string str);
 	
 	public:
 	void		setFirstName(std::string str);
