31b41ea85074e99fc7499140d56e136bab3bce4d
Julian Kersten <jukerste@f1r1s21.codam.nl>
2026-05-28 16:03:43 +0200

    Module 01 is mandatorily done

--- Module_01/ex05/Harl.cpp
+++ Module_01/ex05/Harl.cpp
@@ -1,16 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jul <jul@student.42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/26 01:13:27 by jul               #+#    #+#             */
-/*   Updated: 2026/05/26 02:04:21 by jul              ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   Harl.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jul <jul@student.42.fr>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/05/26 01:13:27 by jul           #+#    #+#                 */
+/*   Updated: 2026/05/28 15:57:08 by jukerste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+Harl::Harl()
+{
+	
+}
+
+Harl::~Harl()
+{
+	
+}
+void	Harl::debug(void)
+{
+	std::cout << "[DEBUG]\nI love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month" << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now." << std::endl;
+}
 
 void	Harl::complain(std::string level)
 {
@@ -40,6 +69,7 @@ void	Harl::complain(std::string level)
 			(this->*complaints[i])();
 			return ;
 		}
+		i++;
 	}
-	std::cout << " Complaining about something.. " << std::endl;
+	std::cout << "[OTHER_MESSAGE]\nComplaining about something.. " << std::endl;
 }
--- Module_01/ex05/main.cpp
+++ Module_01/ex05/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jul <jul@student.42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/05/26 01:12:50 by jul               #+#    #+#             */
-/*   Updated: 2026/05/26 01:13:21 by jul              ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jul <jul@student.42.fr>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/05/26 01:12:50 by jul           #+#    #+#                 */
+/*   Updated: 2026/05/28 15:53:35 by jukerste      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,17 @@
 
 int	main(void)
 {
+	Harl	harl;
 	
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+	harl.complain("I want a discount!");
+	std::cout << std::endl;
 	return (0);
 }
