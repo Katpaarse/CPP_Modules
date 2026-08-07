0162122f601743f8597648a7e0009e998bb69044
Julian Kersten <jukerste@f1r1s19.codam.nl>
2026-08-07 18:14:24 +0200

    ex00 module 05 done

---
+++ Module_05/ex00/Include/Bureaucrat.hpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jukerste <jukerste@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/07/17 14:06:09 by jukerste      #+#    #+#                 */
+/*   Updated: 2026/08/07 17:45:45 by jukerste      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class Bureaucrat
+{
+	private:
+		std::string const _name;
+		int			_grade;
+	public:
+		class GradeTooHighException : public std::exception
+		{
+			public:
+			virtual const char* what() const noexcept
+			{
+				return ("Grade is too high! Max grade is 1");
+			}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+			virtual const char* what() const noexcept
+			{
+				return ("Grade is too low! Max grade is 150");
+			}	
+		};
+		
+		void incrementGrade();
+		void decrementGrade();
+
+		Bureaucrat();
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(Bureaucrat const &other);
+		Bureaucrat& operator=(Bureaucrat const & other);
+		~Bureaucrat();
+		
+		std::string const getName() const;
+		int getGrade() const;
+};
+
+std::ostream& operator<<(std::ostream& output, Bureaucrat const& toPrint);
---
+++ Module_05/ex00/main.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jukerste <jukerste@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/08/07 14:29:09 by jukerste      #+#    #+#                 */
+/*   Updated: 2026/08/07 18:13:23 by jukerste      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main()
+{
+    std::cout << "TEST 1: Normal Creating & Printing" << std::endl;
+    try
+    {
+        Bureaucrat bob("Bob", 75);
+        std::cout << bob << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+    std::cout << "\nTEST 2: Invalid Creation (Grade too high: 0)" << std::endl;
+    try
+    {
+        Bureaucrat high("CEO", 0);
+        std::cout << high << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+    std::cout << "\nTEST 3: Invalid Creation (Grade too low: 151)" << std::endl;
+    try
+    {
+        Bureaucrat low("Intern", 151);
+        std::cout << low << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+    std::cout << "\nTEST 4: Incrementing to the limit and more (Grade: 2 + 1 = 1 + 1 = 0)" << std::endl;
+    try
+    {
+        Bureaucrat alice("Alice", 2);
+        std::cout << alice << std::endl;
+        
+        alice.incrementGrade();
+        std::cout << alice << std::endl;
+
+        alice.incrementGrade();
+        std::cout << alice << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+    std::cout << "\nTEST 5: Decrementing to the absolute limit (Grade: 149 + 1 = 150 + 1)" << std::endl;
+    try
+    {
+        Bureaucrat charlie("Charlie", 149);
+        std::cout << charlie << std::endl;
+        
+        charlie.decrementGrade();
+        std::cout << charlie << std::endl;
+
+        charlie.decrementGrade();
+        std::cout << charlie << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception caught: " << e.what() << std::endl;
+    }
+    return (0);
+}
